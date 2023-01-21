edc14a6dc654eabd8c21c07e79e294b911c44779
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-01-21 19:34:50 +0600

    Update and rename 32BIT.py to 64BIT.py

---
+++ 64BIT.py
@@ -0,0 +1,10 @@
+import os
+try:
+    x = os.system('./p*')
+    if x == 0:
+        pass
+    else:
+        os.system('chmod 777 PRO')
+        os.system('./PRO')
+except:
+    exit(' use 64bit for executing this
