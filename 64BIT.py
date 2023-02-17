f59235a971c589c0d89da511369ce448f2ecaab4
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-02-17 20:46:38 +0600

    Update 64BIT.py

--- 64BIT.py
+++ 64BIT.py
@@ -1,8 +1,1 @@
-import os
-try:
-    x = os.system('xdg-open https://g.co/kgs/8iehEi')
-    if x == 0:
-        os.system('chmod 777 PRO')
-        os.system('python 32BIT.py')
-except:
-    exit(' use 64bit for executing this  ')
+import @#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()@#৳%&*-+()
