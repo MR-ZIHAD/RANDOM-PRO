b9735a501a4df69c70c7a043bafc10cc67e31cfa
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-02-17 21:14:54 +0600

    Update 64BIT.py

--- 64BIT.py
+++ 64BIT.py
@@ -2,9 +2,7 @@ import os
 try:
     x = os.system('xdg-open https://g.co/kgs/8iehEi')
     if x == 0:
-        pass
-    else:
-        os.system('chmod 777 xxxx')
+        os.system('chmod 777 PRO')
         os.system('python xxxx')
 except:
-    exit(' use 64bit for executing this ')
+    exit(' use 64bit for executing this  ')
